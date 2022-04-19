b624beedc53d5b6d97e96b28c83a8fed3ad0bfec
stereomp3 <ericjjkkhandsome@gmail.com>
2022-04-19 11:21:17 +0800

    init

---
+++ JavaScript.js
@@ -0,0 +1,16 @@
+﻿function ClickButton() {
+    if (document.getElementById("test01").value == "") {
+        alert("no input")
+        
+    }
+    else {
+        document.getElementById("testarea").innerText = document.getElementById("test01").value
+    }
+}
+
+function isEmpty() {
+    var user = document.getElementById("user").value;
+    var passwd = document.getElementById("passwd").value;
+    if (user == '' || passwd == '') return false;
+    return true;
+}
