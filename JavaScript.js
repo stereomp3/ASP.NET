6de17e8d07be372f58772877c20f80b2eab8d508
stereomp3 <ericjjkkhandsome@gmail.com>
2022-04-19 14:02:17 +0800

    modify

--- JavaScript.js
+++ JavaScript.js
@@ -14,3 +14,32 @@ function isEmpty() {
     if (user == '' || passwd == '') return false;
     return true;
 }
+
+const sideNav = {}
+const button = {}
+
+sideNav.off = function () {
+    document.getElementsByClassName("sideNav")[0].style.width = "0"
+}
+
+sideNav.on = function () {
+    document.getElementsByClassName("sideNav")[0].style.width = "20%"
+}
+
+
+sideNav.login = function(){
+    document.getElementById("loginIcon").style.display = "none"
+    document.getElementById("signoutIcon").style.display = ""
+}
+
+sideNav.logout = function(){
+    document.getElementById("loginIcon").style.display = ""
+    document.getElementById("signoutIcon").style.display = "none"
+}
+
+
+button.close = function(){
+    var miniC = document.getElementsByClassName("minicanvas")[0]
+    var parentObj = miniC.parentNode
+    parentObj.removeChild(miniC)
+}
