61d6f007032ba25f56ae90375338ff0cf521fa1e
stereomp3 <ericjjkkhandsome@gmail.com>
2022-06-11 17:04:15 +0800

    update

--- JavaScript.js
+++ JavaScript.js
@@ -27,14 +27,13 @@ const button = {}
 sideNav.off = function () {
     document.getElementsByClassName("sideNav")[0].style.width = "0"
     document.querySelector("main").style.width = "100%"
-    document.querySelector("iframe").style.width = "100%"
 }
 
 sideNav.on = function () {
     document.getElementsByClassName("sideNav")[0].style.width = "20%"
     document.querySelector("main").style.width = "80%"
-    document.querySelector("iframe").style.width = "80%"
 }
+
 
 
 sideNav.login = function(){
